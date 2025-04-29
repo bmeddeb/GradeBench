51e430f01a2306bffc1803b113b5e2a98dff055b
Ben Meddeb <bmeddeb@asu.edu>
2025-04-29 08:23:32 -0700

    starlette for static files

--- gradebench/asgi.py
+++ gradebench/asgi.py
@@ -9,21 +9,34 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 
 import os
 from django.core.asgi import get_asgi_application
-from django.conf import settings
+from starlette.staticfiles import StaticFiles
+from starlette.applications import Starlette
 from core.db import database
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gradebench.settings')
 
-# Get the ASGI application
-application = get_asgi_application()
+django_app = get_asgi_application()
+
+# Create a Starlette app to serve static files and mount Django
+app = Starlette()
+
+# Mount static files at /static
+app.mount('/static', StaticFiles(directory=os.path.join(
+    os.path.dirname(__file__), '..', 'static')), name='static')
+
+# Mount Django at root
+app.mount('/', django_app)
 
 # Add database connection lifecycle hooks
+
+
+@app.on_event("startup")
 async def app_startup():
     await database.connect()
 
+
+@app.on_event("shutdown")
 async def app_shutdown():
     await database.disconnect()
 
-# Attach lifecycle hooks to the application
-application.on_startup = [app_startup]
-application.on_shutdown = [app_shutdown]
+application = app
