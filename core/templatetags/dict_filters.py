fb05add8bac6430597bbbe939f56869c0c21c84f
Ben Meddeb <bmeddeb@asu.edu>
2025-05-14 08:06:22 -0700

    feat: added a template tag to ge an item from a dictionary for templates

---
+++ core/templatetags/dict_filters.py
@@ -0,0 +1,44 @@
+"""
+Template filters for dictionaries
+"""
+
+import json as json_lib
+from django import template
+
+register = template.Library()
+
+@register.filter
+def get_item(dictionary, key):
+    """
+    Gets an item from a dictionary for templates
+    Example usage: {{ mydict|get_item:item_key }}
+    """
+    if not dictionary:
+        return None
+    
+    if not key:
+        return None
+    
+    return dictionary.get(str(key))
+
+@register.filter
+def json(value):
+    """
+    Convert a Python object to JSON string for JavaScript use
+    Example usage: {{ my_dict|json }}
+    """
+    return json_lib.dumps(value)
+
+@register.simple_tag
+def get_taiga_config_value(taiga_config, group_id, field_name):
+    """
+    Get a value from nested taiga configuration dictionary.
+    Example usage: {% get_taiga_config_value taiga_config group.id "instance" as instance_value %}
+    """
+    if not taiga_config or not str(group_id) in taiga_config:
+        return ""
+        
+    group_config = taiga_config.get(str(group_id), {})
+    if isinstance(group_config, dict):
+        return group_config.get(field_name, "")
+    return ""
